360f7ee8a653674e566a57424fc82ade6a474171
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-21 13:38:51 +0300

    Make drafts of endpoints for articles

--- mine_backend/app/articlesapi/routes.py
+++ mine_backend/app/articlesapi/routes.py
@@ -17,3 +17,19 @@ def summary():
         return jsonify(TheFlowScraper.get_article_summary(link))
     else:
         abort(400, 'You should provide link query parameter')
+
+
+@bp.route('/article/<id>', methods=['POST'])
+def save_article(id):
+    return jsonify({
+        'id': id,
+        'method': 'POST',
+    })
+
+
+@bp.route('/article/<id>', methods=['GET'])
+def get_article(id):
+    return jsonify({
+        'id': id,
+        'method': 'GET',
+    })
